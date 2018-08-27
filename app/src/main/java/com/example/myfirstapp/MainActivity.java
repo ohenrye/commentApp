ab6d80478a41d769d74f4a6f46131479d5993608
kingfishar <ek.chibueze@gmail.com>
2018-08-27 14:11:43 +0300

    i added android studio comment line.

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //comment
         // new comment
+        //android studio
     }
 }
